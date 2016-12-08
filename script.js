4659744d547b7f387fa38e2ecb663e4dfa0006c6
David Taylor Jr <davidtaylorjr1985@gmail.com>
2016-12-08 11:34:27 -0500

    Clean up script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   $(function() {
     for (var x = 0; x < 16; x++) {
         for (var y = 0; y < 16; y++) {
-            $("<div>").addClass("unit").appendTo("#container");
+            $("<div>").addClass("unit").appendTo('#container');
         }
     }
-  });
-
+});
 });
