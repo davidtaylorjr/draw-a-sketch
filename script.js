ca4a898fa11e81e271bed33ba5d44381905e388b
David Taylor Jr <davidtaylorjr1985@gmail.com>
2016-12-08 12:36:17 -0500

    added hover to the script.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-  $(function() {
-    for (var x = 0; x < 16; x++) {
-        for (var y = 0; y < 16; y++) {
-            $("<div>").addClass("unit").appendTo('#container');
-        }
+ $(function() {
+   for (var x = 0; x < 16; x++) {
+    for (var y = 0; y < 16; y++) {
+        $("<div>").addClass("unit").appendTo('#container');
     }
+ }
 });
-});
+ $(".unit").hover() {
+    $(this).css("background-color", "black");
+  });
+ });
