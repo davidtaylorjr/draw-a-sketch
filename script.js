f21c1d6942c7fe3417e3dc09f100d213da741a7c
David Taylor Jr <davidtaylorjr1985@gmail.com>
2016-12-08 11:22:56 -0500

    Add jquery code for grid and unit class in css
    Found code snippet at
    http://stackoverflow.com/questions/31793518/trying-to-create-a-16x16-grid-of-divs-using-jquery-but-divs-keep-overlapping
    
    Working code sample at http://jsfiddle.net/ge8zd3jv/2/

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
-    
-    
+  $(function() {
+    for (var x = 0; x < 16; x++) {
+        for (var y = 0; y < 16; y++) {
+            $("<div>").addClass("unit").appendTo("#container");
+        }
+    }
+  });
+
 });
