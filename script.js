2c6f5d1964277a968b36ad662ae6c405820cfe8e
David Taylor Jr <davidtaylorjr1985@gmail.com>
2016-12-10 18:58:57 -0500

    Add new Javascript and .css styles
    Current Grid solutions at http://jsfiddle.net/davidtaylorjr/cLcbxmnb/4/

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-$(document).ready(function() {
-  for (var x = 0; x < 16 * 16; x++) {
-    $('<div class="unit">').appendTo('#container');
-  }
+(function(){
 
-  $(".unit").mouseenter(function() {
-    $(this).addClass('black');
-  });
-});
+    //Invoke original function
+   	drawBoxes();
+    
+    //Draw .box divs
+    function drawBoxes(){
+        var gridWidthInBoxes = prompt("Width of grid in boxes?");
+        //Remove boxes and redraw
+        $('.box').remove();
+        for(var i = 0; i < (gridWidthInBoxes * gridWidthInBoxes) ; i++){
+        	$(".container").append("<div class='box'></div>");
+        }
+        //Restyle boxes after they have been drawn
+        restyle(gridWidthInBoxes);
+    }
+    
+    //Style .box divs with appropriate width
+    function restyle(numberofBoxes){
+        //Set width and height css value for '.box' divs
+        $('.box').css('width', (100/numberofBoxes)+'%');
+        $('.box').css('height', (100/numberofBoxes)+'%');
+    }
+    
+    //Button event handler
+    $("button").on('click', function(){
+  		drawBoxes();
+    });
+    
+})();
