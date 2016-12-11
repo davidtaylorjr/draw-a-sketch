f185e4ecfbaa6b32614a73fe8992041d205f96e2
David Taylor Jr <davidtaylorjr1985@gmail.com>
2016-12-10 20:33:00 -0500

    Add New scriptings
    http://codepen.io/davidtaylorjr/pen/KNBJmE

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-(function(){
+$(function(){
 
     //Invoke original function
    	drawBoxes();
-    
+
     //Draw .box divs
     function drawBoxes(){
         var gridWidthInBoxes = prompt("Width of grid in boxes?");
@@ -14,17 +14,17 @@
         //Restyle boxes after they have been drawn
         restyle(gridWidthInBoxes);
     }
-    
+
     //Style .box divs with appropriate width
     function restyle(numberofBoxes){
         //Set width and height css value for '.box' divs
         $('.box').css('width', (100/numberofBoxes)+'%');
         $('.box').css('height', (100/numberofBoxes)+'%');
     }
-    
+
     //Button event handler
     $("button").on('click', function(){
   		drawBoxes();
     });
-    
+
 })();
